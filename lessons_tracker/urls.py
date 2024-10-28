56171fb0e59dfaf562afc7d59ed982baac79d9f7
Simon Leconte <simon.leconte@qantev.com>
2024-10-28 23:27:09 +0100

    add authentication urls

--- lessons_tracker/urls.py
+++ lessons_tracker/urls.py
@@ -24,4 +24,5 @@ import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("lessons_tracker.lessons.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
