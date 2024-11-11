fa7f883b18af405edecb8b729e552ed445277180
Simon Leconte <simon.leconte@qantev.com>
2024-11-11 22:51:00 +0100

    add summary by month

--- lessons_tracker/lessons/serializers/summary.py
+++ lessons_tracker/lessons/serializers/summary.py
@@ -5,3 +5,13 @@ class LessonStatisticsSerializer(serializers.Serializer):
     total_lessons = serializers.IntegerField()
     total_paid = serializers.FloatField()
     total_to_be_paid = serializers.FloatField()
+    total_time = serializers.DurationField()
+
+
+class LessonsStatisticsByMonthSerializer(serializers.Serializer):
+    month = serializers.IntegerField()
+    year = serializers.IntegerField()
+    total_lessons = serializers.IntegerField()
+    total_paid = serializers.FloatField()
+    total_to_be_paid = serializers.FloatField()
+    total_time = serializers.DurationField()
