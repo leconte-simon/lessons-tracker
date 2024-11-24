0d1a32f27ce745ca9f6009bb5595203fa8e61944
Simon Leconte <simon.leconte@qantev.com>
2024-11-24 22:56:30 +0100

    add unique constraint on many to many relationship

--- lessons_tracker/lessons/tests/factories.py
+++ lessons_tracker/lessons/tests/factories.py
@@ -2,7 +2,13 @@ import factory
 import factory.fuzzy
 from factory.django import DjangoModelFactory
 
-from lessons_tracker.lessons.models import ClassLevel, Student, ClassSubject, Lesson
+from lessons_tracker.lessons.models import (
+    ClassLevel,
+    Student,
+    ClassSubject,
+    Lesson,
+    LessonToSubjectRelation,
+)
 
 import datetime as dt
 
@@ -36,10 +42,17 @@ class LessonFactory(DjangoModelFactory):
     class Meta:
         model = Lesson
 
-    subject = factory.SubFactory(ClassSubjectFactory)
     student = factory.SubFactory(StudentFactory)
     datetime = factory.fuzzy.FuzzyDateTime(
         start_dt=dt.datetime(2022, 1, 1, tzinfo=dt.timezone.utc)
     )
     price = factory.fuzzy.FuzzyFloat(low=0, high=100)
     paid = factory.fuzzy.FuzzyChoice([True, False])
+
+
+class LessonToSubjectRelationFactory(DjangoModelFactory):
+    class Meta:
+        model = LessonToSubjectRelation
+
+    lesson = factory.SubFactory(LessonFactory)
+    subject = factory.SubFactory(ClassSubjectFactory)
