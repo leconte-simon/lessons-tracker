d87d9eaa146e8cb4a73fe7cf486ca90729a5fac4
Simon Leconte <simon.leconte@qantev.com>
2024-11-24 22:45:19 +0100

    make lesson subjects a many to many relationship

--- lessons_tracker/lessons/serializers/lesson.py
+++ lessons_tracker/lessons/serializers/lesson.py
@@ -1,8 +1,12 @@
-from lessons_tracker.lessons.models import Lesson
+from lessons_tracker.lessons.models import Lesson, ClassSubject
 from rest_framework import serializers
 
 
 class LessonSerializer(serializers.ModelSerializer):
+    subjects = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=ClassSubject.objects
+    )
+
     class Meta:
         model = Lesson
         fields = "__all__"
